b4255303d54b47352d680fec770a9f1464747e2a
ariesjian <2890283899@qq.com>
2019-04-16 12:37:28 +0800

    球体旋转

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,7 @@
 import Taro, {Component, Config} from '@tarojs/taro'
-import {View,} from '@tarojs/components'
+// 引入 MovableArea, MovableView 组件
+import { MovableArea, MovableView,View } from '@tarojs/components'
 import './index.less'
-
 interface HomeState {
 
 }
@@ -13,7 +13,7 @@ export default class Index extends Component<{}, HomeState> {
     navigationBarTextStyle: 'white',
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#000'
-  }
+  };
 
   constructor(props) {
     super(props)
@@ -24,16 +24,48 @@ export default class Index extends Component<{}, HomeState> {
   };
 
   componentWillMount() {
+
   }
 
   componentDidMount(callback?: any) {
 
   }
+// 开始滑动
+  bindchange = (e) => {
+    console.log(e, '开始滑动')
+  };
+  // 纵向
+  touchmove= (e) => {
+    console.log(e, '纵向')
+  };
+  // 横向
+  touchend= (e) => {
+    console.log(e, '横向')
+  };
 
   render() {
     return (
       <View className='home_container'>
-        index
+
+        <MovableArea style={{width: '400px', height: '400px', margin: '50px auto',}}>
+          <MovableView
+            direction='all'
+            style={{width: '400px', height: '400px',background:'blue'}}
+            bindchange={this.bindchange}
+          >
+          <View className='star_box'>
+            <View className='boll_box'>
+              <View className="circle circle1">1</View>
+              <View className="circle circle2">2</View>
+              <View className="circle circle3">3</View>
+              <View className="circle circle4">4</View>
+              <View className="circle circle5">5</View>
+              <View className="circle circle6">6</View>
+            </View>
+          </View>
+          </MovableView>
+        </MovableArea>
+        <View className='wish'>许愿</View>
       </View>
     )
   }
