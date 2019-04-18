6392adae943e9695a2053b37357307ec601a89fc
ariesjian <2890283899@qq.com>
2019-04-18 09:28:50 +0800

    拖拉星星

--- src/pages/matchdata/index.tsx
+++ src/pages/matchdata/index.tsx
@@ -6,9 +6,10 @@ import './index.less'
 interface StarState {
   starList: Array<any>,
   disabled?: boolean,
-  leftStart:any,
-  topStart:any,
+  leftStart: any,
+  topStart: any,
 }
+
 @connect(({home}) => ({
   ...home,
   home,
@@ -18,25 +19,33 @@ export default class Star extends Component<{}, StarState> {
     navigationBarTitleText: 'start',
     enablePullDownRefresh: false,
   }
+  private position: { x: number; y: number };
+
   constructor(props: {} | undefined) {
     super(props);
     const starList = [
-      {id: 0, left: '150rpx', top: '120rpx', x: 10, y: 10 },
-      {id: 1, left: '140rpx', top: '100rpx', x: 10, y: 10 },
-      {id: 2, left: '400rpx', top: '190rpx', x: 10, y: 10 },
-      {id: 3, left: '300rpx', top: '103rpx', x: 10, y: 10},
-      {id: 4, left: '50rpx', top: '603rpx', x: 10, y: 10}];
+      {id: 0, left: '150rpx', top: '120rpx'},
+      {id: 1, left: '140rpx', top: '100rpx'},
+      {id: 2, left: '400rpx', top: '190rpx'},
+      {id: 3, left: '300rpx', top: '103rpx'},
+      {id: 4, left: '50rpx', top: '603rpx'}];
     this.state = {
       starList: this.styleStart(starList),
       disabled: false,
-      leftStart:0,
-      topStart:0,
+      leftStart: 0,
+      topStart: 0,
+    };
+
+    this.position = {
+      x: -293,
+      y: -238,
     }
   }
 
   componentWillMount() {
 
   }
+
   /*  componentDidMount() {
       const { dispatch } = this.props;
       dispatch({
@@ -53,67 +62,56 @@ export default class Star extends Component<{}, StarState> {
       })
     }*/
   // 星星位置的随机定位处理
-  styleStart=(arr)=>{
-    return arr.map(v=>{
-      return{
+  styleStart = (arr) => {
+    return arr.map(v => {
+      return {
         ...v,
-        left:Math.floor(Math.random()*(707-293)+293),// 首屏随机值
-        top:Math.floor(Math.random()*(762-238)+238)// 首屏随机值
+        left: Math.floor(Math.random() * (707 - 293) + 293),// 首屏随机值
+        top: Math.floor(Math.random() * (762 - 238) + 238)// 首屏随机值
       }
     })
   };
 // 拖动星星开始
-   tounchstart=(e)=>{
-     console.log(e,'===start===')
-      this.setState({
-        disabled: true,
-        leftStart:this.state.starList[e.currentTarget.id].left,
-        topStart:this.state.starList[e.currentTarget.id].top,
-      });
-    }
-// 拖动星星中
-  tounchmove=(e)=>{
-     console.log(e,'===ing===');
-    let xx = 0, yy = 0;
-    const {leftStart,topStart,starList}=this.state;
-    const xEnd = e.changedTouches[0].pageX ;//获取鼠标移动时第一个点的坐标
-    const yEnd = e.changedTouches[0].pageY;
-    xx += xEnd - leftStart;//获得鼠标移动的距离
-    yy += yEnd - topStart;
-    let newArr=starList;
-    newArr[e.currentTarget.id].left=xx;
-    newArr[e.currentTarget.id].top=yy;
-    //将旋转角度写入transform中
-    console.log(newArr,e.currentTarget.id,'------newArr----', newArr[e.currentTarget.id].left)
+  touchStart = (e) => {
+    console.log(e, '===start===',this.position)
     this.setState({
       disabled: true,
-      starList:newArr,
+    });
+  }
+// 拖动星星中
+  touchMove = (e) => {
+    // console.log(e, '===ing===', this.position);
+    if (!e.changedTouches.length) return;
+    const left = e.changedTouches[0].pageX - this.position.x;
+    const top = e.changedTouches[0].pageY - this.position.y;
+    const {starList} = this.state;
+    let newArr = [...starList];
+    newArr[e.currentTarget.id].left = left;
+    newArr[e.currentTarget.id].top = top;
+    console.log(this.position, left, top, '--');
+    this.setState({
+      disabled: true,
+      starList: newArr,
     })
 
   }
- // 拖动星星结束
-  touchend=(e)=>{
-    let xx = 0, yy = 0;
-    const {leftStart,topStart,starList}=this.state;
-    const xEnd = e.changedTouches[0].pageX ;//获取鼠标移动时第一个点的坐标
-    const yEnd = e.changedTouches[0].pageY;
-    xx += xEnd + leftStart;//获得鼠标移动的距离
-    yy += yEnd + topStart;
-    let newArr=starList;
-    newArr[e.currentTarget.id].left=xx;
-    newArr[e.currentTarget.id].top=yy;
-    //将旋转角度写入transform中
+  // 拖动星星结束
+  touchEnd = (e) => {
+    console.log(e, '===end===');
     this.setState({
       disabled: false,
-      starList:newArr,
     })
-  }
+  };
+  onChange=(e) => {
+  this.position = e.detail
+};
+
   render() {
     const {starList, disabled} = this.state;
-    const {
-      home: {homeList},
-    } = this.props;
-    console.log(starList, '---')
+    // const {
+    //   home: {homeList},
+    // } = this.props;
+    // console.log(starList, '---')
     return (
       <View className='page_container'>
         <View className='star_box'>
@@ -122,19 +120,24 @@ export default class Star extends Component<{}, StarState> {
               className='star_view'
               style='height: 1000px; width: 1000px;'
               direction='all'
-              x={-293}
-              y={-238}
+              x={this.position.x}
+              y={this.position.y}
               out-of-bounds
               disabled={disabled}
+              onChange={this.onChange}
             >
               {starList.length && starList.map((v, index) => {
                 return (
-                  <View id={v.id} className='starts' key={index} style={{left: `${v.left}px`, top: `${v.top}px`}}
-                        onTouchMove={this.tounchmove.bind(this)}
-                        onTouchStart={this.tounchstart.bind(this)}
-                        onTouchEnd={this.touchend.bind(this)}
+                  <View
+                    id={v.id}
+                    className='starts'
+                    key={index}
+                    style={{left: `${v.left}px`, top: `${v.top}px`}}
+                    onTouchMove={this.touchMove}
+                    onTouchStart={this.touchStart}
+                    onTouchEnd={this.touchEnd}
                   >
-                    <Image  src={require('../../assets/starton.png')} className='pic_star'/>
+                    <Image src={require('../../assets/starton.png')} className='pic_star'/>
                   </View>
                 )
               })}
